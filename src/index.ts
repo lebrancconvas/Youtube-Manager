28fca39ea07aede7bcc751f06843c8083ddf681d
lebrancconvas <lebrancconvas@gmail.com>
2023-08-01 21:36:08 +0700

    ✨ feat: update import export

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import { getTotalVideoOnChannel, getAllVideoTitleAndURLs } from "./utils";
-
-async function main() {
-  try {
-    // const totalVideo = await getTotalVideoOnChannel('MorningNewsTV3');
-    // console.log(totalVideo);
-    await getAllVideoTitleAndURLs('tpcoder');
-  } catch(err) {
-    console.error(err);
-  }
-}
-
-main();
+import { getTotalVideoOnChannel, getAllVideoTitleAndURLs, fullScroll } from './utils';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './getTotalVideoOnChannel';
 export * from './getAllVideoTitleAndURLs';
+export * from './fullScroll';
